11b2b0137aa3fe41e3816b35f044fdcf432a162f
Abduallah El-Khodary <63024842+abdullahcodee@users.noreply.github.com>
2024-09-05 14:49:02 +0300

    commited the first created APIView

--- profiles_project/profiles_api_app/views.py
+++ profiles_project/profiles_api_app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def index(request):
-    pass
+"""define hello API view """
+
+class HelloAPIView(APIView):
+    def get(self,request, format=None):
+        """return a list of APIview features"""
+        an_api_view = [
+            'Usese HTTP methods as functions (get, POST, Patch, Put, Delete)',
+            'it is  Similar to traditional Django View',
+            'Gives you the most control over your logic',
+            'Is Mapped manually to URLs'
+        ]
+
+        return Response({
+            'message': 'hello',
+            'an_API_view': an_api_view
+            })
+
