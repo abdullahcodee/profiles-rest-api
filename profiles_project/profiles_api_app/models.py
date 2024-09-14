5af8c386c501c79e89320df50c0fc3c19043cf9f
Abduallah El-Khodary <63024842+abdullahcodee@users.noreply.github.com>
2024-09-14 18:10:51 +0300

    added profile feed item and updated migrations then updated the admin pannel

--- profiles_project/profiles_api_app/models.py
+++ profiles_project/profiles_api_app/models.py
@@ -60,3 +60,10 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
         """django used this in the admin showing fields """
         return f"{self.email}"
 
+class ProfileFeedItem(models.Model):
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    create_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status_text
